e618d6c86dbe50eac1551f08019ffe8c828ef6fe
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2023-08-12 15:30:19 +0700

    projects page

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import SectionHeading from './sectionHeading'
+import { projectsData } from '@/lib/data'
+import Image from 'next/image'
 
 export default function Projects() {
   return (
-    <div>project</div>
+    <section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div>
+        {
+          projectsData.map((project, index) => (
+            <React.Fragment key={index}>
+              <Project {...project} />
+            </React.Fragment>
+          ))
+        }
+      </div>
+    </section>
   )
+}
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({
+  title,
+  description,
+  tags,
+  imageUrl
+}: ProjectProps) {
+  return <section>
+    <h3>{title}</h3>
+    <p>{description}</p>
+    <ul>
+      {
+        tags.map((tag, index) => (
+          <li key={index}>{tag}</li>
+        ))
+      }
+    </ul>
+
+    <Image src={imageUrl} alt={title} quality={95} />
+  </section>
 }
