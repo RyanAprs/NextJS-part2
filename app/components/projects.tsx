00a4c0e347b89e76b4e2f4eccf6b9d7fcd24f5d7
Ryan Adi Prasetyo <ryanadiprasetyo2004@gmail.com>
2023-08-13 15:00:14 +0700

    finishing projects page

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import SectionHeading from './sectionHeading'
 import { projectsData } from '@/lib/data'
-import Image from 'next/image'
+import Project from './project'
 
 export default function Projects() {
   return (
-    <section>
+    <section className='scroll-mt-28' id='projects'>
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {
@@ -18,27 +18,4 @@ export default function Projects() {
       </div>
     </section>
   )
-}
-
-type ProjectProps = (typeof projectsData)[number];
-
-function Project({
-  title,
-  description,
-  tags,
-  imageUrl
-}: ProjectProps) {
-  return <section>
-    <h3>{title}</h3>
-    <p>{description}</p>
-    <ul>
-      {
-        tags.map((tag, index) => (
-          <li key={index}>{tag}</li>
-        ))
-      }
-    </ul>
-
-    <Image src={imageUrl} alt={title} quality={95} />
-  </section>
 }
